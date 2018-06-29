f75a72ddb9f294efe64a0629b9adea39766e6405
Giovanni Paolini <giove.paolini@gmail.com>
2018-06-29 14:32:29 +0200

    Add license to source files.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,22 @@
+# -*- coding: utf-8 -*-
 """
-Module that imports all arithmetic matroid classes.
+Module that imports all arithmetic matroids classes.
+
+Copyright (C) 2018 Giovanni Paolini
+Copyright (C) 2018 Roberto Pagaria
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
 """
+
 from arithmetic_matroid import ArithmeticMatroidMixin, ArithmeticMatroid, LinearArithmeticMatroid, BasisArithmeticMatroid, CircuitClosuresArithmeticMatroid, GraphicArithmeticMatroid, MinorArithmeticMatroid, DualArithmeticMatroid, ToricArithmeticMatroid
--- arithmetic_matroid.sage
+++ arithmetic_matroid.sage
@@ -1,9 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Arithmetic matroids classes.
+
+Copyright (C) 2018 Giovanni Paolini
+Copyright (C) 2018 Roberto Pagaria
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+"""
+
 import itertools
-import networkx as nx
 import operator
-# import sys
 import copy
 from fractions import gcd
+import networkx as nx
 
 from sage.matroids.matroid import Matroid
 from sage.matroids.advanced import *
