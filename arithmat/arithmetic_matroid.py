29b10c6e3408870f8f51204b3287d8f502a6acc0
Roberto Pagaria <roberto.pagaria@gmail.com>
2019-07-23 18:35:13 +0200

    minors

--- arithmat/arithmetic_matroid.py
+++ arithmat/arithmetic_matroid.py
@@ -703,7 +703,6 @@ class ToricArithmeticMatroid(ArithmeticMatroidMixin, Matroid):
 
 
     def minor(self, contractions=[], deletions=[]):
-        print contractions, deletions
         contractions = list(contractions)
         deletions = list(deletions)
 
